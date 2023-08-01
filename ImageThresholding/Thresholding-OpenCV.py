f6aae6443c5cca3485e3fc75f6513bc5f373f947
Roke_13 <45883010+ImRoke@users.noreply.github.com>
2023-08-01 21:20:05 +0530

    Update Thresholding-OpenCV.py

--- ImageThresholding/Thresholding-OpenCV.py
+++ ImageThresholding/Thresholding-OpenCV.py
@@ -1,69 +1,82 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# Required modules
 import cv2
-import numpy as np
-from matplotlib import pyplot as plt
+import argparse
+import matplotlib.pyplot as plt
+
+class ImageThresholding:
+    def __init__(self, image_path, technique, block_size=11, c=2, adaptive=False):
+        self.image_path = image_path
+        self.technique = technique
+        self.block_size = block_size
+        self.c = c
+        self.adaptive = adaptive
+
+    def load_image(self):
+        return cv2.imread(self.image_path, cv2.IMREAD_GRAYSCALE)
+
+    def threshold_image(self, image):
+        if self.technique == "THRESH_BINARY":
+            _, thresholded = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+        elif self.technique == "THRESH_BINARY_INV":
+            _, thresholded = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+        elif self.technique == "THRESH_TRUNC":
+            _, thresholded = cv2.threshold(image, 127, 255, cv2.THRESH_TRUNC)
+
+        elif self.technique == "THRESH_TOZERO":
+            _, thresholded = cv2.threshold(image, 127, 255, cv2.THRESH_TOZERO)
+
+        elif self.technique == "THRESH_TOZERO_INV":
+            _, thresholded = cv2.threshold(image, 127, 255, cv2.THRESH_TOZERO_INV)
+
+        elif self.technique == "AdaptiveMean":
+            thresholded = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, \
+                                               cv2.THRESH_BINARY, self.block_size, self.c)
+
+        elif self.technique == "AdaptiveGaussian":
+            thresholded = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
+                                               cv2.THRESH_BINARY, self.block_size, self.c)
+
+        else:
+            raise ValueError("Invalid thresholding technique. Please select from the given options.")
+
+        return thresholded
+
+def plot_thresholding_results(image_path, technique, block_size=11, c=2, adaptive=False):
+    thresholding_obj = ImageThresholding(image_path, technique, block_size, c, adaptive)
+    original_image = thresholding_obj.load_image()
+    thresholded_image = thresholding_obj.threshold_image(original_image)
+
+    # Plot the images
+    plt.figure(figsize=(12, 6))
+    plt.subplot(1, 2, 1)
+    plt.imshow(original_image, cmap="gray")
+    plt.title("Original Image")
+    plt.axis("off")
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(thresholded_image, cmap="gray")
+    plt.title(f"Thresholded Image - {technique}")
+    plt.axis("off")
+    
+    plt.savefig('thresholded_OpenCV.png')
+    plt.show()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Perform different image thresholding techniques using OpenCV")
+    parser.add_argument("input_image", help="Path to the input image file")
+    parser.add_argument("technique", choices=["THRESH_BINARY", "THRESH_BINARY_INV", "THRESH_TRUNC",
+                                              "THRESH_TOZERO", "THRESH_TOZERO_INV", "AdaptiveMean",
+                                              "AdaptiveGaussian"],
+                        help="Thresholding technique to use")
+    parser.add_argument("--block_size", type=int, default=11, help="Block size for adaptive thresholding")
+    parser.add_argument("--c", type=int, default=2, help="Constant C for adaptive thresholding")
+    args = parser.parse_args()
+
+    plot_thresholding_results(args.input_image, args.technique, args.block_size, args.c)
 
 
-# To know the available thresholding (THRESH) methods in OpenCV
-# tmp = [i for i in dir(cv2) if i.startswith('THRESH')]
-# print(f"No of available templates are: {len(tmp)}")
-# print(tmp)
+# Usage
 
-
-# To read only a specified channel from the image, one can use 
-# 0 - grayscale, 1 - color, -1 - unchanged in imread method
-im = cv2.imread('goku3.jpeg', 0)
-
-# Binary threshold method
-ret, bthresh = cv2.threshold(im , 69, 255, cv2.THRESH_BINARY)
-
-# Binary inverse threshold method
-ret, bithresh = cv2.threshold(im, 69, 255, cv2.THRESH_BINARY_INV)
-
-# Truncation threshold method
-ret, tthresh = cv2.threshold(im, 69, 255, cv2.THRESH_TRUNC)
-
-# Threshold to zero method
-ret, zthresh = cv2.threshold(im, 69, 255, cv2.THRESH_TOZERO)
-
-# Threshold to zero inverse method
-ret, zithresh = cv2.threshold(im , 69, 255, cv2.THRESH_TOZERO_INV)
-
-# Plotting 
-names = ['Original','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
-imgs = [img, bthresh, bithresh, tthresh, zthresh, zithresh]
-
-plt.figure(figsize=(25,10))
-
-for i in range(6):
-    plt.subplot(2,3,i+1)
-    plt.imshow(imgs[i],'gray')
-    plt.title(names[i], fontsize = 20)
-    plt.xticks([])
-    plt.yticks([])
-plt.show()
-
-
-# Adaptive thresholding methods
-
-
-# Adaptive Mean Thresholding
-amthresh = cv.adaptiveThreshold(im, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 13, 2)
-
-# Adaptive Gaussian Thresholding
-agthresh = cv.adaptiveThreshold(im, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 13, 2)
-
-# Plotting 
-titles = ['Original', 'Adaptive Mean Threshold', 'Adaptive Gaussian Threshold']
-images = [im, amthresh, agthresh]
-
-plt.figure(figsize = (25,15))
-for i in range(3):
-    plt.subplot(1,3,i+1)
-    plt.imshow(images[i],'gray')
-    plt.title(titles[i], fontsize = 20)
-    plt.xticks([]),plt.yticks([])
-plt.show()
+# python Thresholding-OpenCV.py input_image_path thresholding_technique [--block_size BLOCK_SIZE] [--c C]
+# replace the thresholding_technique with the technique you needed
